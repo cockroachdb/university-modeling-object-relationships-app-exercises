b2db0197ce241d4c759d603910a1afdde7520c88
Alex Yarosh <yarosh@cockroachlabs.com>
2022-08-01 10:21:46 -0400

    fix joins ex

--- solutions/03-many-to-one-second-table/vehicles/get_upcoming_maintenance.sql
+++ solutions/03-many-to-one-second-table/vehicles/get_upcoming_maintenance.sql
@@ -2,4 +2,4 @@ SELECT v.*, ms.maintenance_frequency
 FROM movr_vehicles.vehicles AS v 
 JOIN movr_vehicles.maintenance_schedule AS ms 
   ON v.make = ms.make AND v.model = ms.model 
-WHERE ms.maintenance_frequency - v.last_maintenance < 50;
+WHERE v.mileage - v.last_maintenance > ms.maintenance_frequency;
